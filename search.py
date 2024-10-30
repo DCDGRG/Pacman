5b041bbc6d31d59024882e474b2cebfb886b6183
Dawen.W <dawener@air.local>
2024-10-30 13:22:52 -0700

    Astar

--- search.py
+++ search.py
@@ -148,6 +148,8 @@ def breadthFirstSearch(problem: SearchProblem) -> List[Directions]:
 def uniformCostSearch(problem: SearchProblem) -> List[Directions]:
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    
+
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None) -> float:
@@ -159,53 +161,18 @@ def nullHeuristic(state, problem=None) -> float:
 
 
 # f = g + h, g:从起点到当前节点的实际路径代价, h:从当前节点到目标节点的曼哈顿距离
+# 按照bfs的基本结构的话， 问题就是已经访问的节点无法再次访问， 有最优解但导致路径中断
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic) -> List[Directions]:
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
 
     from util import PriorityQueue
 
-    #  # 初始化优先队列和访问集合
-    # open_list = PriorityQueue()
-    # visited = set()
-
-    # # 获取起始状态并初始化优先级
-    # start_position = problem.getStartState()
-    # start_priority = heuristic(start_position, problem)  # f = g + h，初始 g = 0
-
-    # # 将起始状态压入优先队列
-    # open_list.push((start_position, []), start_priority)
-
-    # # 开始搜索循环
-    # while not open_list.isEmpty():
-    #     currentNode = open_list.pop()
-    #     position = currentNode[0]
-    #     path = currentNode[1]
-
-    #     # 如果是目标状态，返回路径
-    #     if problem.isGoalState(position):
-    #         return path
-
-    #     # 如果当前状态未访问过，进行扩展并标记为已访问
-    #     if position not in visited:
-    #         visited.add(position)
-
-    #         # 获取所有后继节点
-    #         for successor, action, stepCost in problem.getSuccessors(position):
-    #             new_path = path + [action]
-    #             g = problem.getCostOfActions(new_path)  # 累积代价 g(n)
-    #             h = heuristic(successor, problem)  # 启发式代价 h(n)
-    #             f = g + h  # 总代价 f(n) = g(n) + h(n)
-
-    #             # 将后继节点压入优先队列
-    #             open_list.push((successor, new_path), f)
-
-
     #create an priorityQueue to store the state and path
     pq = PriorityQueue()
-    # 存储已经访问过的状态，字典存储，访问过的之后可能还会再访问
-    visited = set()
-    priority = 0 #吗的逼事这个问题
+    # 存储已经访问过的状态，字典存储，键值对：节点和节点的最短路径
+    visited = {}
+    # priority = 0 # 这个不知道为啥影响那么大
 
     # 定义起始状态
     startPosition = problem.getStartState()
@@ -213,7 +180,8 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic) -> List[Directi
 
     # 将起始状态压入队列,路径为空
     pq.push((startPosition, []), startPriority)  # (位置, 路径, 优先级)
-    print(pq.pop())  # 查看返回值的结构  ('A', [])
+    visited[startPosition] = 0;
+    #print(pq.pop())  # 查看返回值的结构  ('A', [])
 
     # 开始搜索
     while not pq.isEmpty():
@@ -227,24 +195,26 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic) -> List[Directi
         if problem.isGoalState(position):
             return path;
         #如果该状态没有访问过，标记之后，进行扩展
-        if position not in visited:
-            visited.add(position)   #标记该状态为已访问
+        # if position not in visited:
+        #     visited.add(position)   #标记该状态为已访问
 
-            # 获取当前节点的所有后继节点
-            successors = problem.getSuccessors(position)
+        # 获取当前节点的所有后继节点
+        successors = problem.getSuccessors(position)
 
-            # 要计算后继节点的cost，那么位置就得更新后再计算，得到cost即priority之后，入列
-            for successor, action, stepCost in successors:
+        # 要计算后继节点的cost，那么位置就得更新后再计算，得到cost即priority之后，入列
+        for successor, action, stepCost in successors:
                  
-                 # move first
-                 newPath = path + [action]
-                 newPosition = successor
+            # move first
+            newPath = path + [action]
+            newPosition = successor
 
-                 h = heuristic(newPosition, problem)
-                 g = problem.getCostOfActions(newPath)
-                 f = g + h
-                 pq.push((successor,newPath), f)
+            h = heuristic(newPosition, problem)
+            g = problem.getCostOfActions(newPath)
+            f = g + h
 
+            if successor not in visited or g< visited[successor]:
+                visited[successor] = g     # # 更新访问记录中的最小代价
+                pq.push((successor,newPath), f)
 
     util.raiseNotDefined()
 
